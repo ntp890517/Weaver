3f33c85b256d37cf9c57938d985c946091713d5c
Shuo-Ren, Lin <ntp890517.ee96@g2.nctu.edu.tw>
2015-08-22 23:26:06 +0800

    minor

--- Weaver/Weaver/urls.py
+++ Weaver/Weaver/urls.py
@@ -21,8 +21,8 @@ from views import main, index, home, design
 urlpatterns = [
     url(r'^$', index),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^index/$', index, name='index'),
+    url(r'^index/$', index),
     url('^', include('django.contrib.auth.urls')),
-    url(r'^home/$', home, name='home'),
-    url(r'^design/$', design, name='design'),
+    url(r'^home/$', home),
+    url(r'^design/$', design),
 ]
